47102a6f635cb64714dd7a82928e8b336696fa65
RumbleFrog <xxlosttechxx@gmail.com>
2018-02-24 20:06:53 -0500

    Spawn Game

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const spawn = require('child_process').spawn
-    , config = require('config.json')
+    , config = require('./config.json')
     , rcon = require('rcon')
     , log = require('winston')
 
@@ -7,9 +7,23 @@ log.addColors({ error: "red", warning: "yellow", info: "green", verbose: "white"
 
 log.remove(log.transports.Console);
 
-log.add(log.transports.Console, { level: config.settings.debug_level, prettyPrint: true, colorize: true, timestamp: true });
+log.add(log.transports.Console, { level: config.debug_level, prettyPrint: true, colorize: true, timestamp: true });
 
-const game = spawn(config.game_binary_location, [[
+const RP = Math.random().toString(36).substring(2);
+
+const game = spawn(config.game_binary_location, [
+  `-game ${config.game}`,
+  '-novid',
   '-usercon',
-  `+rcon_password ${Math.random().toString(36).substring(2)}`
-], ... config.launch_options])
+  `+map ${config.starting_map}`,
+  `+rcon_password ${RP}`,
+  ... config.launch_options
+]);
+
+game.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+game.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
